269203d17e8136e204d24371ece990f0ae66a356
EpicSquid <epicsquid315@gmail.com>
2023-07-31 11:09:52 +1000

    This should work

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,6 +1,6 @@
 object Versions {
 	const val JUNIT = "4.13"
-	const val EXPOSED = "0.41.1"
+	const val EXPOSED = "0.42.0"
 	const val SCHEMA_CRAWLER_VERSION = "16.19.7"
-	const val KOTLIN = "1.8.10"
+	const val KOTLIN = "1.9.0"
 }
